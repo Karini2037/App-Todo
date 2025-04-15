386064ac7e86d8bc04f33b6b405900308603bf47
Karini Sousa <131774110+Karini2037@users.noreply.github.com>
2025-04-14 22:09:17 -0300

    criando um app em react

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, {userEfect, useState} from 'react';
+import {View, Text, FlatList, TextInput, TouchableOpacity, StyleSheet, KeyboardAvoidingView, Plataform} from 'react-native';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3000';
+
+export default function App() {
+    return (
+        'Olá Mundo!'
+    )
+}
